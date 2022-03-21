603d41795af83ad94f204c30be3092ad579b7369
Alan Norton <alan@parallelmarkets.com>
2022-03-21 12:54:51 -0400

    upgrade to 0.8.13

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,7 +26,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: '0.8.13',
     settings: {
       optimizer: {
         enabled: process.env.OPTIMIZE === '1',
