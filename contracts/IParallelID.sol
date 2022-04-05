95eda41a641127ca86f94932352b283ef470b2f4
Brian Muller <bamuller@gmail.com>
2022-04-05 08:43:27 -0400

    relax compiler version requirement to ^0.8.0

--- contracts/IParallelID.sol
+++ contracts/IParallelID.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/IERC721MetadataUpgradeable.sol";
